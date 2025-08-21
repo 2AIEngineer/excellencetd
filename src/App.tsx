a4a49fada7a0ff65a9a4ae82b78bc817757f0c41
2AIEngineer <lifsadev@gmail.com>
2025-08-21 18:13:13 +0100

    V5

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer, type ToastContainerProps } from "react-toastify";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
@@ -11,9 +11,9 @@ import Service from "./pages/Service";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import LegalPage from "./pages/Legal";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
-
 
 export const toastConfig: ToastContainerProps = {
   position: "top-right",
@@ -27,6 +27,13 @@ export const toastConfig: ToastContainerProps = {
   theme: "dark",
 };
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -34,6 +41,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <ToastContainer {...toastConfig} />
+      <ScrollToTop />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
